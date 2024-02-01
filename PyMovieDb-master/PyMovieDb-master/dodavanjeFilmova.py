38bc4fb73dc0fa027f20aa2197b8dba40bad0532
latinbin <157729654+latinbin@users.noreply.github.com>
2024-02-01 19:43:22 +0100

    škola commit

--- PyMovieDb-master/PyMovieDb-master/dodavanjeFilmova.py
+++ PyMovieDb-master/PyMovieDb-master/dodavanjeFilmova.py
@@ -1,23 +1,15 @@
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import os
-uri = "mongodb+srv://francesljas:<password>@cluster0.zadaprp.mongodb.net/"
+
+uri = "mongodb+srv://francesljas:Fran2008.@cluster0.zadaprp.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-def dodaj_film(json):
-    db = client['DBMoovies']
-    moovies = db['Moovies']
-    moovie = moovies.insert_one(json)
-    print(f'Film: {json["name"]}; ID: {moovie.inserted_id}')
-    
-
-def dodaj_filmove(dirPath):
-    for f in os.scandir(dirPath):
-        if not f.is_dir() and f.name[-4:] == 'json':
-            print(f'{dirPath}\\{f.name}')
-            f = open(f'{dirPath}\\{f.name}')
-            data = eval(f.read())
-            f.close()
-            dodaj_film(data)
-
-dodaj_filmove('C:\\Programi')
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
